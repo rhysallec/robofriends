b33115231208846d8b2d20cddfc5ded7025b9900
Rhys Allec <rhysallecbaldemor@gmail.com>
2020-07-28 02:39:14 +0800

    Updates

---
+++ precache-manifest.16b82b24b7922391d19e52ac6794fcb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cf3a4b627501ca60a9990abfc37383",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "01df9987754b9f826405",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01df9987754b9f826405",
+    "url": "/robofriends/static/js/main.c4bb113a.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
